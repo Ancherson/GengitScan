90b0b92e89701c734899b6858de2fcacf52bc212
Yoan Rougeolle <yoyo031201@orange.fr>
2020-10-12 13:19:42 +0200

    TODO: 'save command line options to a file instead of running analysis' done

--- cli/src/main/java/up/visulog/cli/CLILauncher.java
+++ cli/src/main/java/up/visulog/cli/CLILauncher.java
@@ -4,7 +4,13 @@ import up.visulog.analyzer.Analyzer;
 import up.visulog.config.Configuration;
 import up.visulog.config.PluginConfig;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Optional;
 
@@ -41,10 +47,16 @@ public class CLILauncher {
                             break;
                         case "--loadConfigFile":
                             // TODO (load options from a file)
-                            break;
+                        	break;
+                        	
                         case "--justSaveConfigFile":
                             // TODO (save command line options to a file instead of running the analysis)
-                            break;
+                        	//Format command: --justSaveConfigFile=Path to file where you want save the command
+                        	//Example: ./gradlew run --args='--addPlugin=countCommits --justSaveConfigFile=../config.txt'
+                        	//this command saves "--addPlugin=countCommits" in ../config.txt
+                        	String command = saveConfig(args,pValue);
+                        	System.out.println("Command: " + command + ", is saved");
+                        	System.exit(0);
                         default:
                             return Optional.empty();
                     }
@@ -55,6 +67,24 @@ public class CLILauncher {
         }
         return Optional.of(new Configuration(gitPath, plugins));
     }
+    
+    //this function save the command in the file whose path is "path"
+    private static String saveConfig(String[] args, String path) {
+    	String content = "";
+    	for(int i = 0; i < args.length; i++) {
+    		if(!args[i].equals("--justSaveConfigFile=" + path)) content += args[i] + " ";
+    	}
+    	content = content.substring(0, content.length() - 1);
+    	try {
+    		FileWriter file = new FileWriter(path);
+    		file.write(content);
+    		file.close();
+    		return content;
+    	} catch(IOException e) {
+    		throw new RuntimeException("Error SaveConfig", e);
+    	}
+    }
+    
 
     private static void displayHelpAndExit() {
         System.out.println("Wrong command...");
