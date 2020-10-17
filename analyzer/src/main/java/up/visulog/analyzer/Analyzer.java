aee8754c084bff3d5b284d7c87e95e73090b7d3b
Elody <elodytang@hotmail.fr>
2020-10-17 17:44:14 +0200

    add the command plugin

--- analyzer/src/main/java/up/visulog/analyzer/Analyzer.java
+++ analyzer/src/main/java/up/visulog/analyzer/Analyzer.java
@@ -46,6 +46,9 @@ public class Analyzer {
         switch (pluginName) {
             case "countCommits" : return Optional.of(new CountCommitsPerAuthorPlugin(config));
             case "countMergeCommits" : return Optional.of(new CountMergeCommitsPerAuthorPlugin(config));
+            case "countCommitsPerMonths" : return Optional.of(new CommitsPerDatePlugin(config));
+            case "countCommitsPerDays" : return Optional.of(new CommitsPerDatePlugin(config, "days"));
+            case "countCommitsPerWeeks" : return Optional.of(new CommitsPerDatePlugin(config, "weeks"));
             default : return Optional.empty();
         }
     }
