1975e622959680c4ed07daf3945e98a64809c762
Elody <elodytang@hotmail.fr>
2020-10-21 15:53:59 +0200

    somes changes but still weird

--- analyzer/src/main/java/up/visulog/analyzer/CommitsPerDatePlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CommitsPerDatePlugin.java
@@ -29,41 +29,50 @@ public class CommitsPerDatePlugin implements AnalyzerPlugin {
     	var result = new Result();
     	result.setHowToSort(howToSort);
     	if(howToSort == "months") { //Commits per Months
-	        for (var commit : gitLog) {
-	        	String m = commit.date.getMonth().name();
-	        	var nb = result.commitsPerDate.getOrDefault(m, 0);
-	        	LocalDateTime datetmp = commit.date;
-	        	if(commit.date.getMonth() == datetmp.getMonth()) {
-	        		result.commitsPerDate.put(m, nb + 1);
-	        	}            
-	        }
-	        return result;
+	        sortPerMonth(gitLog, result);
         }
-    	if(howToSort == "days") { //Commits per Days
-	        for (var commit : gitLog) {
-	        	String m = Integer.toString(commit.date.getDayOfMonth()) + " " + commit.date.getMonth().name();
-	        	var nb = result.commitsPerDate.getOrDefault(m, 0);
-	        	LocalDateTime datetmp = commit.date;
-	        	if(commit.date.getDayOfMonth() == datetmp.getDayOfMonth()) {
-	        		result.commitsPerDate.put(m, nb + 1);
-	        	}            
-	        }
-	        return result;
+    	if(howToSort == "days") { //Commits per Days    		
+    		sortPerDays(gitLog, result);
         }
     	if(howToSort == "weeks") { //Commits per Weeks
-	        for (var commit : gitLog) {
-	        	String m = "Week " + Integer.toString(getNumberOfWeek(commit.date));
-	        	var nb = result.commitsPerDate.getOrDefault(m, 0);
-	        	LocalDateTime datetmp = commit.date;
-	        	if(getNumberOfWeek(commit.date) == getNumberOfWeek(datetmp)) {
-	        		result.commitsPerDate.put(m, nb + 1);
-	        	}
-	        }
-	        return result;
+    		sortPerWeeks(gitLog, result);
         }
-    	
     	return result;
     }
+    
+    //function fills the result variable with the way of sorting
+    static void sortPerMonth(List<Commit> gitLog, Result result) {
+    	for (var commit : gitLog) {
+        	String m = commit.date.getMonth().name();
+        	var nb = result.commitsPerDate.getOrDefault(m, 0);
+        	LocalDateTime datetmp = commit.date;
+        	if(commit.date.getMonth() == datetmp.getMonth()) {
+        		result.commitsPerDate.put(m, nb + 1);
+        	}            
+        }
+    }
+    
+    static void sortPerDays(List<Commit> gitLog, Result result) {
+    	for (var commit : gitLog) {
+        	String m = Integer.toString(commit.date.getDayOfMonth()) + " " + commit.date.getMonth().name();
+        	var nb = result.commitsPerDate.getOrDefault(m, 0);
+        	LocalDateTime datetmp = commit.date;
+        	if(commit.date.getDayOfMonth() == datetmp.getDayOfMonth()) {
+        		result.commitsPerDate.put(m, nb + 1);
+        	}            
+        }
+    }
+
+	static void sortPerWeeks(List<Commit> gitLog, Result result) {
+		for (var commit : gitLog) {
+        	String m = Integer.toString(getNumberOfWeek(commit.date));
+        	var nb = result.commitsPerDate.getOrDefault(m, 0);
+        	LocalDateTime datetmp = commit.date;
+        	if(getNumberOfWeek(commit.date) == getNumberOfWeek(datetmp)) {
+        		result.commitsPerDate.put(m, nb + 1);
+        	}
+        }
+	}
     
     //function which returns the number of the week
     static int getNumberOfWeek(LocalDateTime date) {
@@ -82,12 +91,13 @@ public class CommitsPerDatePlugin implements AnalyzerPlugin {
         if (result == null) run();
         return result;
     }
+   
+ 
     
-    //
     
     
     static class Result implements AnalyzerPlugin.Result {
-        private final Map<String, Integer> commitsPerDate = new HashMap<>();
+        private Map<String, Integer> commitsPerDate = new HashMap<>();
         private String howToSort = "month";
         
         public void setHowToSort(String howToSort) {
@@ -97,6 +107,62 @@ public class CommitsPerDatePlugin implements AnalyzerPlugin {
         Map<String, Integer> getCommitsPerDate() {
             return commitsPerDate;
         }
+        
+        // strange function that still sorts the months... ?????
+        //to fix
+        public void sort() {
+        	String[] months = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
+        	
+        	if(this.howToSort.equals("months")) { //sort per months
+        		/*int[] tab = new int[commitsPerDate.size()];
+        		int indice = 0;
+        		for(var item : commitsPerDate.entrySet()) {
+        			String key = item.getKey();
+        			for(int i=0; i<months.length; i++) {
+        				if(key.equals(months[i])) {
+        					tab[indice] = i;
+        					indice++;
+        				}
+        			}
+        		}
+        		boolean change = false;
+        		do {
+        			change = false;
+        			for(int i=0; i<tab.length-1; i++) {
+        				if(tab[i]>tab[i+1]) {
+        					int tmp = tab[i];
+        					tab[i] = tab[i+1];
+        					tab[i+1] = tmp;
+        					change = true;
+        				}
+        			}
+        		} while(change);*/
+        		Map<String, Integer> copy = new HashMap<>();
+        		copy.putAll(commitsPerDate); //copie de la map
+        		commitsPerDate.clear(); //on efface la map pour la re remplir
+        
+        		/*for(var item : copy.entrySet()) {
+        			System.out.println(item.getKey() + ": " + item.getValue());
+        		}
+        		
+        		for(int i=0; i<tab.length; i++) {
+        			indice = tab[i];
+        			commitsPerDate.put(months[indice], copy.get(months[indice]));
+        			System.out.println();
+        			for(var item : commitsPerDate.entrySet()) {
+            			System.out.println(item.getKey());
+            		}
+        			System.out.println();
+        		}*/
+        		commitsPerDate = copy;
+        		
+        		
+        	}	
+        }
+        
+        
+        
+        
 
         @Override
         public String getResultAsString() {
@@ -105,9 +171,14 @@ public class CommitsPerDatePlugin implements AnalyzerPlugin {
 
         @Override
         public String getResultAsHtmlDiv() {
+        	sort();
             StringBuilder html = new StringBuilder("<div>Number of Commits per " + this.howToSort + ": <ul>");
             for (var item : commitsPerDate.entrySet()) {
-                html.append("<li>").append(item.getKey()).append(": ").append(item.getValue()).append("</li>");
+            	if(this.howToSort.equals("weeks")) {
+            		html.append("<li>Week ").append(item.getKey()).append(": ").append(item.getValue()).append("</li>");
+            	} else {
+            		html.append("<li>").append(item.getKey()).append(": ").append(item.getValue()).append("</li>");
+            	}
             }
             html.append("</ul></div>");
             return html.toString();
