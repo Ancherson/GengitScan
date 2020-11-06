e378d3c6a9b1a0a8f5830facb2f2fbdbbd9f672b
zhangxxu <1569316914zxw@gmail>
2020-11-06 20:01:41 +0100

    2eme ajoute des codes du plugin pour CountCommitsPerAuthorPerDay

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPerDayPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPerDayPlugin.java
@@ -34,3 +34,27 @@ public class CountCommitsPerAuthorPerDayPlugin implements AnalyzerPlugin {
         if (result == null) run();
         return result;
     }
+
+    static class Result implements AnalyzerPlugin.Result {
+        private final Map<String, Integer> commitsPerAuthor = new HashMap<>();
+
+        Map<String, Integer> getCommitsPerAuthor() {
+            return commitsPerAuthor;
+        }
+
+        @Override
+        public String getResultAsString() {
+            return commitsPerAuthor.toString();
+        }
+
+        @Override
+        public String getResultAsHtmlDiv() {
+            StringBuilder html = new StringBuilder("<div>Commits per author: <ul>");
+            for (var item : commitsPerAuthor.entrySet()) {
+                html.append("<li>").append(item.getKey()).append(": ").append(item.getValue()).append("</li>");
+            }
+            html.append("</ul></div>");
+            return html.toString();
+        }
+    }
+}
