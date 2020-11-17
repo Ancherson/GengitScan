5e5e79c249b8a63f5441303e2a3de3e74d9a543b
zhangxxu <1569316914zxw@gmail>
2020-11-17 10:34:32 +0100

    modifier le plugin pour CountCommitsPerAuthorPerWeek

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPerWeekPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPerWeekPlugin.java
@@ -14,3 +14,25 @@ public class CountCommitsPerAuthorPerWeekPlugin implements AnalyzerPlugin {
     public CountCommitsPerAuthorPerWeekPlugin(Configuration generalConfiguration) {
         this.configuration = generalConfiguration;
     }
+    
+    static Result processLog(List<Commit> gitLog) {
+        var result = new Result();
+        //rester a modifier
+        //ajouter la date
+        for (var commit : gitLog) {
+            var nb = result.commitsPerAuthorPerDay.getOrDefault(commit.author, 0);
+            result.commitsPerAuthorPerDay.put(commit.author, nb + 1);
+        }
+        return result;
+    }
+
+    @Override
+    public void run() {
+        result = processLog(Commit.parseLogFromCommand(configuration.getGitPath()));
+    }
+
+    @Override
+    public Result getResult() {
+        if (result == null) run();
+        return result;
+    }
