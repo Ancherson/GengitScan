3f942033cd9fd371bab7202b6b73ae5c6475d07e
Elody <elodytang@hotmail.fr>
2020-11-20 16:46:50 +0100

    change names in the plugin CountLinesPerAuthorPerDatePlugin

--- analyzer/src/main/java/up/visulog/analyzer/CountLinesPerAuthorPerDatePlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountLinesPerAuthorPerDatePlugin.java
@@ -164,13 +164,22 @@ public class CountLinesPerAuthorPerDatePlugin implements AnalyzerPlugin {
     		String[] a = commit.author.split(" ");
     		String email = a[a.length-1];
     		if(emailToName.containsKey(email)) {
-    			var name = emailToName.get(email);
+    			String author = emailToName.get(email);
+    			String[] nameTab = author.split(" ");
+    			String name = "";
+    			for(int i=0; i<nameTab.length-1; i++) {
+    				name += nameTab[i] + " ";
+    			}
+    			if(name.equals("BADAOUI ismail ")) {
+        			name = "Ismail BADAOUI ";
+        		}
     			Commit m = new Commit(commit.id, name, commit.date, commit.description, commit.mergedFrom);
     			m.linesAdded = commit.linesAdded;
     			m.linesDeleted = commit.linesDeleted;
     			gitLog2.add(m);
             }
     	}
+    	
     	return gitLog2;
     }
     
