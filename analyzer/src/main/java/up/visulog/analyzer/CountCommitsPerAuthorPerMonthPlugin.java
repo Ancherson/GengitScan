aa619773a4e95f6e68a05ad359e78f7121235c42
zhangxxu <1569316914zxw@gmail>
2020-11-23 22:01:07 +0100

    modifier CountCommitsPerAuthorPerMonthPlugin

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPerMonthPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPerMonthPlugin.java
@@ -4,8 +4,10 @@ import up.visulog.config.Configuration;
 import up.visulog.gitrawdata.Commit;
 
 import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.List;
 import java.util.Map;
+import java.time.*;
 
 public class CountCommitsPerAuthorPerMonthPlugin implements AnalyzerPlugin {
     private final Configuration configuration;
@@ -17,12 +19,57 @@ public class CountCommitsPerAuthorPerMonthPlugin implements AnalyzerPlugin {
 
     static Result processLog(List<Commit> gitLog) {
         var result = new Result();
-        //rester a modifier
-        for (var commit : gitLog) {
-            var nb = result.commitsPerAuthorPerMonth.getOrDefault(commit.author, 0);
-            result.commitsPerAuthorPerMonth.put(commit.author, nb + 1);
+        Map<LocalDate, List<Commit>> commitsPerDay = sortCommitsPerDay(gitLog);
+        for (var commit : commitsPerDay.entrySet()) {
+          LocalDate m = commit.getKey();
+    		  var nb = result.commitsPerAuthorPerDay.getOrDefault(commit.author, 0);
+            result.commitsPerAuthorPerDay.put(commit.author, nb + 1);
         }
-        return result;
+        Map<LocalDate, Map<String, Integer>> res = new TreeMap<>();
+    		// we sort the number of commits per month
+        	for(var date : result.getCommitsPerAuthorPerMonth().entrySet()) {
+            LocalDate m = LocalDate.of(date.getKey().getYear(), date.getKey().getMonth(), 1);
+        		Map<String, Integer> res2 = authorsAndMonths(m, result);
+        		res.put(m, res2);
+        	}
+          result.CommitsPerAuthorPerWeek.putAll(res);
+          return result;
+    }
+
+    //commitsPerDay
+    static Map<LocalDate, List<Commit>> sortCommitsPerDay(List<Commit> gitLog) {
+     Map<LocalDate, List<Commit>> res = new TreeMap<>();
+     for (var commit : gitLog) {
+       LocalDate m = commit.date.toLocalDate();
+         res.put(m, makeCommitsList(m, gitLog));
+       }
+     return res;
+   }
+
+   static List<Commit> makeCommitsList(LocalDate m, List<Commit> gitLog) {
+     List<Commit> list = new LinkedList<>();
+     for (var commit : gitLog) {
+       LocalDate c = commit.date.toLocalDate();
+       if(c.equals(m)) {
+         list.add(commit);
+       }
+       }
+     return list;
+   }
+
+   public Map<String, Integer> authorAndMonths(String months, Result r) {
+     Map<String, Integer> res = new HashMap<>();
+    	for(var date : r.commitsPerAuthorPerDay.entrySet()) {
+    		LocalDate m = LocalDate.of(date.getKey().getYear(), date.getKey().getMonth(), 1);
+    		if(months.equals(m)) {
+    			for(var c : date.getValue().entrySet()) {
+    				String a = c.getKey();
+                	var nb = res.getOrDefault(a, 0);
+                	res.put(a, nb + 1);
+    			}
+    		}
+    	}
+    	return res;
     }
 
     @Override
@@ -37,24 +84,31 @@ public class CountCommitsPerAuthorPerMonthPlugin implements AnalyzerPlugin {
     }
 
     static class Result implements AnalyzerPlugin.Result {
-        private final Map<String, Integer> commitsPerAuthor = new HashMap<>();
+        public final Map<LocalDate, Map<String, Integer>> commitsPerAuthorPerDay = new TreeMap<>();
+        public Map<String, Map<String, Integer>> commitsPerAuthorPerMonth = new TreeMap<>();
 
-        Map<String, Integer> getCommitsPerAuthor() {
-            return commitsPerAuthor;
+        Map<LocalDate, Map<String, Integer>> getCommitsPerAuthorPerMonth() {
+            return commitsPerAuthorPerMonth;
         }
 
         @Override
         public String getResultAsString() {
-            return commitsPerAuthor.toString();
+            return commitsPerAuthorPerMonth.toString();
         }
 
         @Override
         public String getResultAsHtmlDiv() {
-            StringBuilder html = new StringBuilder("<div>Commits per author: <ul>");
-            for (var item : commitsPerAuthor.entrySet()) {
-                html.append("<li>").append(item.getKey()).append(": ").append(item.getValue()).append("</li>");
-            }
-            html.append("</ul></div>");
+            StringBuilder html = new StringBuilder("<div>Commits per author per month: <ul>");
+            String s;
+            for (var item : commitsPerAuthorPerWeek.entrySet()) {
+        			s += "<ul>" + item.getKey().getMonth().name() + " " + item.getKey().getYear() + "<br>";
+        			Map<String, Integer> commits = item.getValue();
+            		for(var c : commits.entrySet()) {
+            			s += "<li>" + c.getKey() + " : " + c.getValue() + "</li><br>";
+            		}
+            		s+= "</ul><br>";
+        		}
+            html.append(s).append("</ul></div>");
             return html.toString();
         }
     }
