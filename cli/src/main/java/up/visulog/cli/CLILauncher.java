7a57daf58a04ced53d35eef16bc0bd5e160b69b1
Ismail BADAOUI <ismail.badaoui@etu.univ-paris-diderot.fr>
2020-11-25 01:28:24 +0100

    Fix some errors

--- cli/src/main/java/up/visulog/cli/CLILauncher.java
+++ cli/src/main/java/up/visulog/cli/CLILauncher.java
@@ -21,6 +21,7 @@ import java.net.URISyntaxException;
 import java.nio.file.FileSystems;
 
 //libraries that allow you to find files thanks to their path
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -225,8 +226,9 @@ public class CLILauncher {
                             return Optional.empty();
                     }
                 }
-            } else {
-                gitPath = FileSystems.getDefault().getPath(arg);
+            } else{
+            	if(Files.exists(Paths.get(arg)))  gitPath = FileSystems.getDefault().getPath(arg);
+				else displayHelpAndExit();
             }
         }
         if(API && (pProjectId==-1 || pPrivateToken.equals(""))){
