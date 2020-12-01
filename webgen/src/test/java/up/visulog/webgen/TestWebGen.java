62c74cf8a39ca69697e3c789c348d7c168c961d7
Yoan Rougeolle <yoyo031201@orange.fr>
2020-12-01 19:03:30 +0100

    Resolve the test

--- webgen/src/test/java/up/visulog/webgen/TestWebGen.java
+++ webgen/src/test/java/up/visulog/webgen/TestWebGen.java
@@ -4,6 +4,13 @@ import up.visulog.webgen.WebGen;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,8 +20,8 @@ import org.junit.Test;
 
 public class TestWebGen {
 
-	//@Test
-	public void checkWebGen() {
+	@Test
+	public void checkWebGen() throws URISyntaxException, IOException {
 		//initializing Map and String for addListAuthor
 		Map<String, Integer>list = new HashMap<String, Integer>();
 		list.put("Author 1",1);
@@ -38,11 +45,18 @@ public class TestWebGen {
 		webG.addListAuthor(list, title);
 		webG.addChart(title, labels, datasets);
 		
-		String result = webG.getHtml();
-		String expected = "";
+		String result = webG.getHtml() + "\n";
+		var uri = getClass().getClassLoader().getResource("truc.html").toURI();
+		var expected = new File(uri);
+		String expec = "";
+		BufferedReader reader = new BufferedReader(new FileReader(expected));
+		String line = "";
+		while((line = reader.readLine()) != null) {
+			expec += line + "\n";
+		}
 		
 		//checking if we get expected result
-		assertEquals(result,expected);
+		assertEquals(result,expec);
 	}	
 
 }
