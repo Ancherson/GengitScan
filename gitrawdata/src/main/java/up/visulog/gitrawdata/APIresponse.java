d9b198f38cbe1f21a91e6444ff225108e506896b
Elody <elodytang@hotmail.fr>
2020-12-15 16:50:47 +0100

    change the error output for API errors

--- gitrawdata/src/main/java/up/visulog/gitrawdata/APIresponse.java
+++ gitrawdata/src/main/java/up/visulog/gitrawdata/APIresponse.java
@@ -217,11 +217,75 @@ public class APIresponse {
     }
     private static void displayHelpAndExit() {
         System.out.println("Wrong command...");
-        //TODO: print the list of options and their syntax
-        System.out.println("Different options: ");
-        System.out.println("--addPlugin allows you to add new plugins");
-        System.out.println("--loadConfigFile allows you to load options from a file");
-        System.out.println("--justSaveConfigFile save command line options to a file instead of running the analysis");
-        System.exit(0);
+        System.out.println("Here is a list of what we can do : ");
+	    printAllPossiblePlugins();
+	    System.out.println("--load= (Loads the selected commands (test in the example) into a file ../config.txt)");
+	    System.out.println("(Format command: --load=name of the config)");
+	    System.out.println("Example : ./gradlew run --args='--load=test'\n");
+	    
+	    System.out.println("--save= (Save the commande \"--addPlugin=(name of command)\" in ../config.txt)");
+	    System.out.println("(Format command: --save=name of the config)");
+	    System.out.println("Example : ./gradlew run --args='--addPlugin=countCommits --save=test'\n");
+	    
+	    System.out.println("--privateToken= (to use the API)");
+	    System.out.println("(For our project: 1m1pdKszBNnTtCHS9KtS)\n");
+	    System.out.println("--projectId= (this is a ID of the project)");
+	    System.out.println("(For our project: 1618)\n");
+	    System.exit(0);
     }
+
+    private static void printAllPossiblePlugins() {
+    	String space = "          ";
+    	String prohibition = " (X)";
+    	String api = " (API)";
+    	System.out.println("--addPlugin= (allows you to add new plugins)");
+    	System.out.println("If you want to see the results on all branches, just add \"ForAllBranches\" at the end of the command.\n");
+    	
+    	System.out.println("\"(X)\" means you can't have this command on all branches.\n");
+    	System.out.println("\"(API)\" means that the plugin in question uses an API. You will have to add as an option \"privateToken\" and projectId\".\n");
+    	
+    	System.out.println("GENERAL STATISTICS :");
+    	System.out.println("getMembers" + space + "Get members" + prohibition + api);
+    	System.out.println();
+    	
+    	System.out.println("ACTIVITY :");
+    	System.out.println("countCommitsPerDays" + space + "Commits per days");
+    	System.out.println("countCommitsPerWeeks" + space + "Commit per weeks");
+    	System.out.println("countCommitsPerMonths" + space + "Commits per months");
+    	System.out.println("countLinesAddedPerDays"+ space + "Lines added per days");
+    	System.out.println("countLinesDeletedPerDays"+ space + "Lines deleted per days");
+    	System.out.println("countLinesAddedPerWeeks"+ space + "Lines added per weeks");
+    	System.out.println("countLinesDeletedPerWeeks"+ space + "Lines deleted per weeks");
+    	System.out.println("countLinesAddedPerMonths"+ space + "Lines added per months");
+    	System.out.println("countLinesDeletedPerMonths"+ space + "Lines deleted per months");
+    	System.out.println();
+    	
+    	System.out.println("AUTHOR :");
+    	System.out.println("countCommits" + space + "Commits by author");
+    	System.out.println("countMergeCommits" + space + "Merge Commits by author" + prohibition);
+    	System.out.println("countLinesAdded" + space + "Lines added by author");
+    	System.out.println("countLinesDeleted" + space + "Lines deleted by author");
+    	System.out.println("countContribution"+ space + "Authors' contribution" + prohibition);
+    	System.out.println("countComments" + space + "Comments by author" + prohibition + api);
+    	System.out.println("countIssues" + space + "Issues by author" + prohibition + api);
+    	System.out.println();
+    	
+    	System.out.println("ACTIVITY BY AUTHOR :");
+    	System.out.println("countLinesAddedPerAuthorPerDays"+ space + "Lines added by author and per days");
+    	System.out.println("countLinesDeletedPerAuthorPerDays" + space + "Lines deleted by author and per days");
+    	System.out.println("countLinesAddedPerAuthorPerWeeks" + space + "Lines added by author and per weeks");
+    	System.out.println("countLinesDeletedPerAuthorPerWeeks" + space + "Lines deleted by author and per weeks");
+    	System.out.println("countLinesAddedPerAuthorPerMonths" + space + "Lines added by author and per months");
+    	System.out.println("countLinesDeletedPerAuthorPerMonths" + space + "Lines deleted by author and per months");
+    	System.out.println("countCommitsPerAuthorPerDays"+ space + "Commits by author and per days");
+    	System.out.println("countCommitsPerAuthorPerWeeks" + space + "Commits by author and per weeks");
+    	System.out.println("countCommitsPerAuthorPerMonths" + space + "Commits by author and per months");
+    	System.out.println();
+    	
+    	System.out.println("FILES AND EXTENSION :");
+    	System.out.println("getExtensions" + space + "Get Extensions" + prohibition + api);
+    	System.out.println();       
+    }
+
+
 }
